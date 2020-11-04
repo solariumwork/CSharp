d0005f7b6ed4c098e3bbbc704c7392d4ff65738b
Igor Pavlov <i.pavlov@worksolutions.ru>
2020-11-04 18:17:26 +0300

    Lesson2: add task 3

---
+++ Lesson2.Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lesson2.Task3
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            int sum = 0;
+            int num;
+            
+            Console.WriteLine("Введите числа (0 - завершить ввод)");
+            do
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                if (num > 0 && IsOdd(num))
+                {
+                    sum += num;
+                }
+            } while (num != 0);
+            
+            Console.WriteLine($"\nСумма нечётных положительных чисел: {sum}");
+        }
+
+        static bool IsOdd(int num)
+        {
+            return num % 2 != 0;
+        }
+    }
+}
