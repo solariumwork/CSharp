868dd85e8b4270fc11135826a5c03548895e5a4e
Igor Pavlov <i.pavlov@worksolutions.ru>
2020-11-04 04:07:59 +0300

    Lesson2: add task 2

---
+++ Lesson2.Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lesson2.Task2
+{
+    static class Program
+    {
+        static int NumsCount(float num)
+        {
+            string numString = num.ToString();
+            if (numString.Contains(","))
+            {
+                return numString.Length - 1;
+            }
+
+            return numString.Length;
+        }
+        
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            float num = Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine($"Количество цифр в числе: { NumsCount(num) }");
+        }
+    }
+}
