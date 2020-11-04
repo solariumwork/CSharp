8085d337e645dddfa29ddaaf4dd94da0a8a69988
Igor Pavlov <i.pavlov@worksolutions.ru>
2020-11-04 19:03:23 +0300

    Lesson2: add task 4

---
+++ Lesson2.Task4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Lesson2.Task4.Auth;
+
+namespace Lesson2.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string error;
+            do
+            {
+                Console.WriteLine("Введите логин");
+                string login = Convert.ToString(Console.ReadLine());
+
+                Console.WriteLine("\nВведите пароль");
+                string password = Convert.ToString(Console.ReadLine());
+
+                error = AuthService.Login(login.Trim(), password.Trim());
+                if (error != "")
+                {
+                    Console.WriteLine($"\nОшибка: {error}");
+                }
+                
+                Console.WriteLine("");
+            } while (error != "");
+            
+            Console.WriteLine("Successful login");
+        }
+    }
+}
